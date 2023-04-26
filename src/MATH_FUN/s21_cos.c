a7135a51a3719ea91b6e8347df5bd229a455a602
Dich21 <y.kuklin@gmail.com>
2023-04-27 00:02:24 +0300

    cos

--- src/MATH_FUN/s21_cos.c
+++ src/MATH_FUN/s21_cos.c
@@ -1,1 +1,14 @@
 #include "../s21_math.h"
+
+long double s21_cos(double x) {
+  if (x == S21_PI)
+    return -1;
+  if (x == -S21_PI)
+    return -1;
+
+  long double res = 1;
+
+  res = s21_sin(S21_PI / 2 - x);
+
+  return res;
+}
