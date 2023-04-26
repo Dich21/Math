8560f7f889b52933d323e2d8ee9128896c45b67d
Dich21 <y.kuklin@gmail.com>
2023-04-27 00:28:11 +0300

    tan

--- src/MATH_FUN/s21_tan.c
+++ src/MATH_FUN/s21_tan.c
@@ -1,1 +1,13 @@
+#include <math.h>
+
 #include "../s21_math.h"
+
+long double s21_tan(double x) {
+    if (x == S21_Inf) return S21_NaN;
+
+    x = s21_fmod(x, S21_PI);
+
+    long double res = s21_sin(x) / s21_cos(x);
+
+    return res;
+}
