a83b3c446735041d4f19f201d940d11a07079ba4
Dich21 <y.kuklin@gmail.com>
2023-04-27 21:36:02 +0300

    gitignor

--- src/MISC/animation.c
+++ src/MISC/animation.c
@@ -1,12 +1,12 @@
 #include "../s21_math.h"
 
 int main() {
-    int i;
-    for (i = 0; i < 70; i++) {
-        printf("-");
-        fflush(stdout);
-        usleep(5000);
-    }
-    printf("\n");
-    return 0;
+  int i;
+  for (i = 0; i < 70; i++) {
+    printf("-");
+    fflush(stdout);
+    usleep(5000);
+  }
+  printf("\n");
+  return 0;
 }
